5015b58bb15833fbd1f9d5a97eba51e6eba8072c
Larissa Barros <78032881+larissabants@users.noreply.github.com>
2021-11-15 19:32:10 -0300

    Add files via upload

---
+++ ATIVIDADES/A18/processa.js
@@ -0,0 +1,53 @@
+const fs = require ('fs')
+
+console.log(1)
+
+//callback
+/*
+fs.readFile('./in1.txt', (err, contents) => {
+    fs.readFile('./in2.txt', (err2, contents2) => {
+    console.log(err, String(contents))
+    console.log(err2, String(contents2))
+    })
+})
+*/
+
+//promisse
+const readFile = file => new Promise((resolve, reject) => {
+    fs.readFile(file, (err, contents) => {
+        if(err) {
+            reject(err)
+        } else {
+            resolve(contents)
+        }
+    })
+})
+
+//promisse
+/*
+readFile('./in1.txt')
+    .then(contents => {
+    console.log(String(contents))
+    return readFile('./in2.txt')
+})
+    .then(contents => {
+        console.log(promessa)
+})*/
+
+//async/await - açúcar sintatico em cima da promise
+
+const init = async() => {
+    try {
+        const contents = await readFile('./in1.txt')
+        const contents2 = await readFile('./in2.txt')
+        return String(contents) + '\n' + String(contents2) 
+    } catch(err) {
+    console.log(err)
+    }
+} 
+
+init().then(contents => console.log(contents))
+
+console.log(2)
+
+console.log(3)
