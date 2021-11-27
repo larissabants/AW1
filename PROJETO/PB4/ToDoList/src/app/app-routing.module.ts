6e390f3ae5e3557f196f10aa3b399ec8a0c65a32
Larissabants <larissabants@gmail.com>
2021-11-27 01:29:57 -0300

    add

---
+++ PROJETO/PB4/ToDoList/src/app/app-routing.module.ts
@@ -0,0 +1,16 @@
+import { TaskListComponent } from './tasks/task-list/task-list.component';
+import { NgModule, Component } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { TaskFormComponent } from './tasks/task-form/task-form.component';
+
+const routes: Routes = [
+  { path: '', component: TaskListComponent},
+  { path: 'new', component: TaskFormComponent},
+  { path: 'edit/:id', component: TaskFormComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ PROJETO/PB4/ToDoList/src/app/tasks/task-list-item/task-list-item.component.ts
@@ -0,0 +1,27 @@
+import { TaskService } from './../shared/task.service';
+import { Component, OnInit, Input } from '@angular/core';
+import { Task } from '../shared/task';
+
+@Component({
+  selector: 'app-task-list-item',
+  templateUrl: './task-list-item.component.html',
+  styleUrls: ['./task-list-item.component.css']
+})
+export class TaskListItemComponent implements OnInit {
+  @Input()
+  task: Task = new Task;
+
+  constructor(private taskService: TaskService) { }
+
+  ngOnInit() {
+  }
+
+  remove(task: Task) {
+    this.taskService.delete(task.id);
+  }
+
+  onCompletedCheckChange(task: Task) {
+    this.taskService.save(task);
+  }
+
+}
